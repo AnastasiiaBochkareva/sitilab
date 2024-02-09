e51ec36c1f8191b64825b8a326654843e2235a98
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2024-02-09 20:56:43 +0400

    edit style

--- src/components/Visit-promo/Visit-promo.js
+++ src/components/Visit-promo/Visit-promo.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const visitPromo = document.querySelector('.visit-promo');
-  const tabVisitPromo = visitPromo.closest('.tabb');
-  const parent = tabVisitPromo.parentElement;
-  const heightVisitPromo = tabVisitPromo.clientHeight;
-  parent.style.height = `${heightVisitPromo}px`;
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const visitPromo = document.querySelector('.visit-promo');
+//   const tabVisitPromo = visitPromo.closest('.tabb');
+//   const parent = tabVisitPromo.parentElement;
+//   const heightVisitPromo = tabVisitPromo.clientHeight;
+//   parent.style.height = `${heightVisitPromo}px`;
+// });

7d2eb799dc947a93fd588244f0bdac6dfdf24bbc
AnastasiiaBochkareva <nastia.bochk@gmail.com>
2024-12-10 15:57:05 +0400

    edits main-page

--- src/components/ui/Accordion/Accordion.js
+++ src/components/ui/Accordion/Accordion.js
@@ -14,10 +14,31 @@ const closeItem = (item) => {
   body.style.maxHeight = null;
 };
 
+function fixScroll(activeItem, itemToScroll) {
+  if (!activeItem) return;
+  const body = activeItem.querySelector('.accordion__body');
+  console.log(body.scrollHeight);
+  const viewportHeight = window.innerHeight;
+  console.log(viewportHeight);
+  if (body.scrollHeight > viewportHeight) {
+    setTimeout(() => {
+      const header = document.querySelector('.header');
+      const offset = header ? header.scrollHeight : 100;
+      const rect = itemToScroll.getBoundingClientRect();
+      const offsetTop = window.pageYOffset + rect.top - offset;
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
+    }, 250);
+  }
+}
+
 const clickHandler = (e) => {
   const accordionHead = e.target.closest('.accordion__head');
   if (!accordionHead) return;
-
+  console.log(e);
   const accordion = e.target.closest('.accordion');
   const targetItem = e.target.closest('.accordion__item');
   const activeItem = accordion.querySelector('.accordion__item.active');
@@ -25,6 +46,7 @@ const clickHandler = (e) => {
   if (targetItem !== activeItem) {
     openItem(targetItem);
     closeItem(activeItem);
+    fixScroll(activeItem, targetItem);
   } else {
     closeItem(targetItem);
   }

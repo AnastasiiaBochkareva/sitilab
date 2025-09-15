6ff9d3ec812693518cbce25c6755cc8830c77356
AnastasiiaBochkareva <nastia.bochk@gmail.com>
2025-09-15 19:41:30 +0400

    discard hidden sliders code

--- src/js/slider.js
+++ src/js/slider.js
@@ -8,68 +8,68 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 
-// let objectSliders = [];
-// const saleCards = document.querySelectorAll('.a-sale__cards');
-// if (saleCards) {
-//   saleCards.forEach((element, index) => {
-//     const btnPrev = element.querySelector('.slider-navigation_prev');
-//     const btnNext = element.querySelector('.slider-navigation_next');
-//     const saleSlider = element.querySelector('.a-sale__slider');
-//     const swiperParams = {
-//       modules: [Navigation],
-//       slidesPerView: '3',
-//       spaceBetween: 20,
-//       navigation: {
-//         prevEl: btnPrev,
-//         nextEl: btnNext,
-//       },
-//       observer: true,
-//       observeParents: true,
-//       observeSlideChildren: true,
-//       grabCursor: true,
-//       slideToClickedSlide: true,
-//       breakpoints: {
-//         1440: {
-//           slidesPerView: '3',
-//           spaceBetween: 20,
-//         },
-//         1000: {
-//           slidesPerView: '3',
-//         },
-//         800: {
-//           slidesPerView: '2.1',
-//         },
-//         375: {
-//           slidesPerView: '1.5',
-//           spaceBetween: 10,
-//         },
-//         300: {
-//           slidesPerView: '1.1',
-//           spaceBetween: 10,
-//         },
-//       },
-//     };
-//     if (saleSlider.classList.contains('a-main__slider')) {
-//       swiperParams.breakpoints = {
-//         1001: {
-//           slidesPerView: '3',
-//         },
-//         768: {
-//           slidesPerView: '2',
-//         },
-//         360: {
-//           slidesPerView: '1.5',
-//           spaceBetween: 10,
-//         },
-//         300: {
-//           slidesPerView: '1',
-//           spaceBetween: 10,
-//         },
-//       };
-//     }
-//     objectSliders[index] = new Swiper(saleSlider, swiperParams);
-//   });
-// }
+let objectSliders = [];
+const saleCards = document.querySelectorAll('.a-sale__cards');
+if (saleCards) {
+    saleCards.forEach((element, index) => {
+        const btnPrev = element.querySelector('.slider-navigation_prev');
+        const btnNext = element.querySelector('.slider-navigation_next');
+        const saleSlider = element.querySelector('.a-sale__slider');
+        const swiperParams = {
+            modules: [Navigation],
+            slidesPerView: '3',
+            spaceBetween: 20,
+            navigation: {
+                prevEl: btnPrev,
+                nextEl: btnNext,
+            },
+            observer: true,
+            observeParents: true,
+            observeSlideChildren: true,
+            grabCursor: true,
+            slideToClickedSlide: true,
+            breakpoints: {
+                1440: {
+                    slidesPerView: '3',
+                    spaceBetween: 20,
+                },
+                1000: {
+                    slidesPerView: '3',
+                },
+                800: {
+                    slidesPerView: '2.1',
+                },
+                375: {
+                    slidesPerView: '1.5',
+                    spaceBetween: 10,
+                },
+                300: {
+                    slidesPerView: '1.1',
+                    spaceBetween: 10,
+                },
+            },
+        };
+        if (saleSlider.classList.contains('a-main__slider')) {
+            swiperParams.breakpoints = {
+                1001: {
+                    slidesPerView: '3',
+                },
+                768: {
+                    slidesPerView: '2',
+                },
+                360: {
+                    slidesPerView: '1.5',
+                    spaceBetween: 10,
+                },
+                300: {
+                    slidesPerView: '1',
+                    spaceBetween: 10,
+                },
+            };
+        }
+        objectSliders[index] = new Swiper(saleSlider, swiperParams);
+    });
+}
 
 const askExpertsSlider = new Swiper('.ask-experts__body', {
     modules: [Navigation],
@@ -106,49 +106,49 @@ const askExpertsSlider = new Swiper('.ask-experts__body', {
     },
 });
 
-// const corporateSlider = new Swiper('.corporate-slider', {
-//   modules: [Navigation, Pagination, Autoplay],
-//   slidesPerView: '5',
-//   spaceBetween: 40,
-//   navigation: {
-//     prevEl: document.querySelector('.slider-navigation_prev'),
-//     nextEl: document.querySelector('.slider-navigation_next'),
-//   },
-//   pagination: {
-//     el: '.corporate-pagination',
-//   },
-//   noSwiping: true,
-//   grabCursor: true,
-//   slideToClickedSlide: true,
-//   autoplay:
-//     window.innerWidth <= 768
-//       ? {
-//           delay: 2500,
-//           disableOnInteraction: false,
-//         }
-//       : false,
-//   breakpoints: {
-//     1308: {
-//       slidesPerView: '5',
-//     },
-//     901: {
-//       slidesPerView: '4',
-//       spaceBetween: 20,
-//     },
-//     701: {
-//       slidesPerView: '3',
-//     },
-//     450: {
-//       slidesPerView: '2',
-//     },
-//     400: {
-//       slidesPerView: '1',
-//     },
-//     300: {
-//       slidesPerView: '1',
-//     },
-//   },
-// });
+const corporateSlider = new Swiper('.corporate-slider', {
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: '5',
+    spaceBetween: 40,
+    navigation: {
+        prevEl: document.querySelector('.slider-navigation_prev'),
+        nextEl: document.querySelector('.slider-navigation_next'),
+    },
+    pagination: {
+        el: '.corporate-pagination',
+    },
+    noSwiping: true,
+    grabCursor: true,
+    slideToClickedSlide: true,
+    autoplay:
+        window.innerWidth <= 768
+            ? {
+                  delay: 2500,
+                  disableOnInteraction: false,
+              }
+            : false,
+    breakpoints: {
+        1308: {
+            slidesPerView: '5',
+        },
+        901: {
+            slidesPerView: '4',
+            spaceBetween: 20,
+        },
+        701: {
+            slidesPerView: '3',
+        },
+        450: {
+            slidesPerView: '2',
+        },
+        400: {
+            slidesPerView: '1',
+        },
+        300: {
+            slidesPerView: '1',
+        },
+    },
+});
 // const mainBannerSlider = new Swiper('.main-banner__slider', {
 //   modules: [Pagination, Autoplay],
 //   slidesPerView: '1',

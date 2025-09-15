8780437dd357717b052d689d75d5c4bf4f58f3d7
AnastasiiaBochkareva <nastia.bochk@gmail.com>
2025-09-15 19:34:24 +0400

    change components links

--- src/components/components.js
+++ src/components/components.js
@@ -1,19 +1,19 @@
 // Input
 import '@/components/ui/Input/inputSetFocus';
-// import '@/components/ui/Input/inputSearch';
+import '@/components/ui/Input/inputSearch';
 import '@/components/ui/Input/select';
 import '@/components/ui/Input/textarea';
-// import '@/components/ui/Input/inputFile';
-// import '@/components/ui/Input/inputCount';
+import '@/components/ui/Input/inputFile';
+import '@/components/ui/Input/inputCount';
 
-// import '@/components/ui/Tooltip/tooltip';
-// import '@/components/ui/Menu/Menu';
+import '@/components/ui/Tooltip/tooltip';
+import '@/components/ui/Menu/Menu';
 import '@/components/ui/Accordion/Accordion';
-// import '@/components/ui/СhoosingСity/ChoosingCity';
-// import '@/components/ui/scrollTo/scrollTo';
+import '@/components/ui/СhoosingСity/ChoosingCity';
+import '@/components/ui/scrollTo/scrollTo';
 import '@/components/ui/Tabs/TabsNav';
 
-// import '@/mixins/Header/Header';
+import '@/mixins/Header/Header';
 import '@/mixins/Visit-price/Visit-price';
 
 import '@/components/Visit-promo/Visit-promo';
@@ -27,21 +27,21 @@ import '@/mixins/Main-map/Main-map';
 import '@/mixins/Filter-result/Filter-result';
 import '@/mixins/News-card/News-card';
 import MyModal from '@/components/ui/MyModal/MyModal';
-// import Filters from '@/components/ui/Filters/Filters';
-// import Validation from '@/components/ui/Form/Validation';
-// import Mask from '@/components/ui/Input/mask';
+import Filters from '@/components/ui/Filters/Filters';
+import Validation from '@/components/ui/Form/Validation';
+import Mask from '@/components/ui/Input/mask';
 
 // Input mask
-// const maskInit = Mask;
-// maskInit.init();
+const maskInit = Mask;
+maskInit.init();
 
 // Form validator
-// const validationInit = new Validation();
-// validationInit.init();
+const validationInit = new Validation();
+validationInit.init();
 
 // Modals
 const modalInit = new MyModal();
 modalInit.init();
 
-// const filterInit = new Filters();
-// filterInit.init();
+const filterInit = new Filters();
+filterInit.init();
